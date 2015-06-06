f119ce1fd254669258dcd466129d36ff790df5f7
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-06-06 13:33:27 -0300

    disabling sql logger

--- played_with_me/settings.py
+++ played_with_me/settings.py
@@ -126,24 +126,24 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGGING = {
-    'disable_existing_loggers': False,
-    'version': 1,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',  # message level to be written to console
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'django.db': {},
-    },
-}
+#LOGGING = {
+#    'disable_existing_loggers': False,
+#    'version': 1,
+#    'handlers': {
+#        'console': {
+#            'class': 'logging.StreamHandler',
+#            'level': 'DEBUG',  # message level to be written to console
+#        },
+#    },
+#    'loggers': {
+#        '': {
+#            'handlers': ['console'],
+#            'level': 'DEBUG',
+#            'propagate': False,
+#        },
+#        'django.db': {},
+#    },
+#}
 import logging
 logging.getLogger("requests").setLevel(logging.WARNING)
 
